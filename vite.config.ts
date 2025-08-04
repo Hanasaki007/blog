9037c42d8338ce37de1f10107d46bcc971dc7169
kokoa <2394773855@qq.com>
2025-08-04 15:28:07 +0800

    bugfix

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/vue3-music/',
+  base: '/blog/',
   build: {
     outDir: 'docs'
   }
